ce99286ce4c82b2a6579b36819f12063ecae5f94
Fernando Mayer <fernandomayer@gmail.com>
2019-01-23 14:41:58 -0200

    In the beginning there was darkness

---
+++ _build.R
@@ -0,0 +1,14 @@
+build <- function(ignore.path = "docs", quiet = TRUE){
+    fls <- dir(pattern = "*.rmd$", recursive = TRUE, ignore.case = TRUE)
+    if(length(fls) == 0){ stop("No .Rmd file found") }
+    fls <- grep(ignore.path, fls, value = TRUE, invert = TRUE)
+    fls <- normalizePath(fls)
+    fls.out <- gsub("\\.[Rr]md$", ".pdf", fls)
+    for(i in 1:length(fls)){
+        rmarkdown::render(input = fls[i],
+                          output_file = fls.out[i],
+                          quiet = quiet)
+    }
+}
+
+build()
